9b38d20b247fb97be225dc151cf667b23183c731
Matheus Maia Alvarez <88752578+MatheusAlvarez@users.noreply.github.com>
2022-03-29 09:55:54 -0300

    Add files via upload

---
+++ Seção 3/Seção 3 - 4/Ex001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Informe um valor: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+    }
+}
