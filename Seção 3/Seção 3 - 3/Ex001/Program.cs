40d69630bf64ba9891ec35668e776b7b0726f734
Matheus Maia Alvarez <88752578+MatheusAlvarez@users.noreply.github.com>
2022-03-29 09:54:26 -0300

    Add files via upload

---
+++ Seção 3/Seção 3 - 3/Ex001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe a senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
